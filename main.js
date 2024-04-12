8994f698efc0a1292c6b079d0d65a8416db236eb
LN <leonelngoya@gmail.com>
2024-04-12 04:25:20 +0100

    first version

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-import './style.scss'
-import gsap from 'gsap'
+import "./style.scss";
+import gsap from "gsap";
+
+const images = document.querySelectorAll('img');
+
+const timeline = gsap.timeline({ repeat: -1 });
+
+images.forEach((image, index) => {
+  timeline.to(image, { display: "block", duration: 0, onComplete: () => {
+    images.forEach((img, i) => {
+      if (i !== index) {
+        gsap.set(img, { display: "none" });
+      }
+    });
+  }})
+  .to(image, { display: "none", duration: 0.05 }, "+=0.05");
+});
+
+timeline.play();
